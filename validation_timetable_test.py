ab3b53d464abe34ed78c732e12da7c073c03215b
c-rainbow <c.rainbow.678@gmail.com>
2020-04-04 02:35:57 -0700

    More timetable validation test

--- validation_timetable_test.py
+++ validation_timetable_test.py
@@ -21,7 +21,7 @@ class BaseTestClass(unittest.TestCase):
     def setUp(self):
         self.assignment_dict = {
             (_START_DATE, 'name1'): _DAY_SHIFT, (_END_DATE, 'name1'): _EVENING_SHIFT,
-            (_START_DATE, 'name2'): _DAY_SHIFT, (_END_DATE, 'name2'): _NIGHT_SHIFT,
+            (_START_DATE, 'name2'): _OFF_SHIFT, (_END_DATE, 'name2'): _NIGHT_SHIFT,
         }
         self.person_configs = [
             data.PersonConfig('name1', 5, 3, 1, 30), data.PersonConfig('name2', 6, 4, 2, 20)]
@@ -117,30 +117,88 @@ class BasicValidationTest(BaseTestClass):
 
 
 # Test of validation 4
-class OverAssignmentValidationTest2(unittest.TestCase):
-    def testEmpty(self):
-        pass
+class OverAssignmentValidationTest(BaseTestClass):
+    # ValidateOverassignment(assignment_dict, date_configs, errors, barebone=False)
+    
+    def setUp(self):
+        super().setUp()
+        self.exact_match_date_configs = [
+            data.DateConfig(_START_DATE, 1, 0, 0), data.DateConfig(_END_DATE, 0, 1, 1)
+        ]
+    
+    def testBarebone(self):
+        # incomplete assignment does not cause error for barebone
+        vt.ValidateOverassignment(
+            self.assignment_dict, self.date_configs, self.errors, barebone=True)
+        self.assertEqual(len(self.errors), 0)
+
+        # Exact match also does not cause error for barebone
+        vt.ValidateOverassignment(
+            self.assignment_dict, self.exact_match_date_configs, self.errors, barebone=True)
+        self.assertEqual(len(self.errors), 0)
+
+   
+    def testNonBarebone(self):
+        # Exact match does not cause error for non-barebone
+        vt.ValidateOverassignment(
+            self.assignment_dict, self.exact_match_date_configs, self.errors, barebone=False)
+        self.assertEqual(len(self.errors), 0)
+
+    def testNonBareboneUnderAssignment(self):
+        # Under-assignment causes error for non-barebone
+        date_configs = [
+            data.DateConfig(_START_DATE, 1, 1, 0), data.DateConfig(_END_DATE, 1, 1, 2)
+        ]
+        vt.ValidateOverassignment(self.assignment_dict, date_configs, self.errors, barebone=False)
+        self.assertEqual(len(self.errors), 3)
+        self.assertEqual(
+            self.errors[0], '0 workers are assigned for EVENING on 2020-04-01, when there should be 1 workers')
+        self.assertEqual(
+            self.errors[1], '0 workers are assigned for DAY on 2020-04-02, when there should be 1 workers')
+        self.assertEqual(
+            self.errors[2], '1 workers are assigned for NIGHT on 2020-04-02, when there should be 2 workers')
+
+    def testBareboneOverassignment(self):
+        date_configs = [
+            data.DateConfig(_START_DATE, 0, 1, 1), data.DateConfig(_END_DATE, 1, 1, 2)
+        ]
+        vt.ValidateOverassignment(self.assignment_dict, date_configs, self.errors, barebone=True)
+        self.assertEqual(len(self.errors), 1)
+        self.assertEqual(
+            self.errors[0], '1 workers are assigned for DAY on 2020-04-01, when there should be 0 workers')
+
+    def testNonBareboneOverassignment(self):
+        date_configs = [
+            data.DateConfig(_START_DATE, 0, 0, 0), data.DateConfig(_END_DATE, 0, 0, 1)
+        ]
+        vt.ValidateOverassignment(self.assignment_dict, date_configs, self.errors, barebone=False)
+        self.assertEqual(len(self.errors), 2)
+        self.assertEqual(
+            self.errors[0], '1 workers are assigned for DAY on 2020-04-01, when there should be 0 workers')
+        self.assertEqual(
+            self.errors[1], '1 workers are assigned for EVENING on 2020-04-02, when there should be 0 workers')
 
 
 # Test of validation 5 (constraint 2, 3, 4, 5)
-class ConstraintsValidationTest(unittest.TestCase):
+class ConstraintsValidationTest(BaseTestClass):
     def testEmpty(self):
         pass
 
 
 # Test of validation 6
-class MinRequiredWorkersTest(unittest.TestCase):
+class MinRequiredWorkersTest(BaseTestClass):
     pass
 
 
 # Test of validation 7
-class MinTotalWorkSlotsTest(unittest.TestCase):
+class MinTotalWorkSlotsTest(BaseTestClass):
     pass
 
 
 # Test of validation 8
-class MaxTotalWorkSlotsTest(unittest.TestCase):
+class MaxTotalWorkSlotsTest(BaseTestClass):
     pass
+
 
 if __name__ == '__main__':
     unittest.main()
