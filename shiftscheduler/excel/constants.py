781979eafd06d540394907b260996544050fe7e7
c-rainbow <c.rainbow.678@gmail.com>
2020-04-24 18:32:12 -0700

    Generated all .pot files

--- shiftscheduler/excel/constants.py
+++ shiftscheduler/excel/constants.py
@@ -1,23 +1,24 @@
 """Constants for Excel"""
 
-import gettext
-_ = gettext.gettext
+from shiftscheduler.i18n import gettext
+
+_ = gettext.GetTextFn('excel/constants')
 
 
 # Config values for each person. The order must be preserved
 CONFIG_PERSON_HEADER_NAMES = (
-    _('최대 근무일 (연속)'),
-    _('최대 나이트 (연속)'),
-    _('최소 근무일 (전체)'),
-    _('최대 근무일 (전체)'),
+    _('Max consecutive workdays'),
+    _('Max consecutive nights'),
+    _('Min total workdays'),
+    _('Max total workdays'),
 )
 
 
 # Config values for each date. The order must be preserved
 CONFIG_DATE_HEADER_NAMES = (
-    _('데이 근무자 수'),
-    _('이브닝 근무자 수'),
-    _('나이트 근무자 수'),
+    _('Number of day workers'),
+    _('Number of evening workers'),
+    _('Number of night workers'),
 )
 
 
@@ -29,7 +30,7 @@ COLOR_OFF = 'bbbbbb'  # Light gray
 
 
 # Sheet names in Excel files
-SHEET_TIMETABLE = _('일정표')
-SHEET_PERSON_CONFIG = _('간호사별 설정')
-SHEET_DATE_CONFIG = _('날짜별 설정')
-SHEET_SOFTWARE_CONFIG = _('Config')
+SHEET_TIMETABLE = _('Timetable')
+SHEET_PERSON_CONFIG = _('Person Config')
+SHEET_DATE_CONFIG = _('Date Config')
+SHEET_SOFTWARE_CONFIG = _('Software Config')
